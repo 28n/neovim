bfcbbaba40dc82d2c7770a745c047563a3dba01e
28n <28nh04@gmail.com>
2023-01-07 15:13:04 +0100

    feat(plugins): add chadtree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,10 @@
 vim.g.mapleader = " "
 
 if vim.fn.has("nvim-0.8") == 1 then
-  vim.opt.backup = true
-  vim.opt.cmdheight = 0
-  vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup"
+	vim.opt.backup = true
+	vim.opt.cmdheight = 0
+	vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup"
 end
 
 vim.g.node_host_prog = "/Users/folke/.pnpm-global/5/node_modules/neovim/bin/cli.js"
+vim.g.python3_host_prog = "C:/Windows/py.exe"
