e3313d16839136bdbc02afb9904a92f94ec54c25
Nick Habelsberger <nick@habelsberger.com>
2023-06-19 16:02:11 +0200

    ja

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,15 +1,11 @@
-local use_dev = true
 
-if use_dev then
-  vim.opt.runtimepath:prepend(vim.fn.expand("~/projects/lazy.nvim"))
-else
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
-    vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
-  end
-  vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
 end
+vim.opt.rtp:prepend(lazypath)
+
 
 ---@param opts LazyConfig
 return function(opts)
